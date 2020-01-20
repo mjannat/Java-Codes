802c62334bfdf76fd9fe68e715b00ee7fb0eef53
mjannat <30448145+mjannat@users.noreply.github.com>
2020-01-20 16:21:53 +0600

    Practicing Java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package datasoft;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bismillahir Rahmanir Rahim\n");
+        System.out.println("Say Alhamdulillah");
+        System.out.println("Hard Work pays off");
+        System.out.println("Best of Luck MJ :)");
+    }
+    
+}
